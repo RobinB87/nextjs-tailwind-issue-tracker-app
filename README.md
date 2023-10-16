aaac477a1be1acbbb61cc48d72bce0d9a62a1aec
RobinB87 <robinbette@hotmail.com>
2023-10-16 15:25:27 +0000

    add some styling and an icon via react-icons

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # nextjs-tailwind-issue-tracker-app
 
 1. npx create-next-app
+2. npm install react-icons --save
--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
+import { BsFillBugFill } from "react-icons/bs";
+
 const NavBar = () => (
-  <nav>
-    <Link href="/">Logo</Link>
-    <ul>
+  <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+    <Link href="/">
+      <BsFillBugFill />
+    </Link>
+    <ul className="flex space-x-6">
       <li>
         <Link href="/">Dashboard</Link>
       </li>
