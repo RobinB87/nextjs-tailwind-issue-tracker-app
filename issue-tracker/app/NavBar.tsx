334d7b731872defd0decdd60e8a02ff3c927bc05
RobinB87 <robinbette@hotmail.com>
2023-10-16 15:31:39 +0000

    finalize navbar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -2,20 +2,33 @@ import Link from "next/link";
 
 import { BsFillBugFill } from "react-icons/bs";
 
-const NavBar = () => (
-  <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-    <Link href="/">
-      <BsFillBugFill />
-    </Link>
-    <ul className="flex space-x-6">
-      <li>
-        <Link href="/">Dashboard</Link>
-      </li>
-      <li>
-        <Link href="/issues">Issues</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <BsFillBugFill />
+      </Link>
+      <div className="flex space-x-6">
+        {links &&
+          links.map((x) => (
+            <Link key={x.href} href={x.href} className="text-zinc-500 hover:text-zinc-800 transition-colors">
+              {x.label}
+            </Link>
+          ))}
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
