35e30329d82601fe722ee975a2ef544f0f735f0f
RobinB87 <robinbette@hotmail.com>
2023-10-26 14:33:23 +0000

    apply single responsibility principle

---
+++ issue-tracker/app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import { BsPencilSquare } from "react-icons/bs";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button>
+      <BsPencilSquare />
+      <Link href={`/issues/${issueId}/id`}>Edit issue</Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
