7091b7831b4b3ac98154d39f4e9dd52262eea9ea
RobinB87 <robinbette@hotmail.com>
2023-10-27 17:06:57 +0000

    configure google provider for auth

--- issue-tracker/app/api/issues/auth/[...nextauth]/route.ts
+++ issue-tracker/app/api/issues/auth/[...nextauth]/route.ts
@@ -1,7 +1,17 @@
 import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const clientId = process.env.GOOGLE_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+if (!clientId || !clientSecret) throw new Error("Google ClientId and ClientSecret are required");
 
 const handler = NextAuth({
-  providers: [],
+  providers: [
+    GoogleProvider({
+      clientId,
+      clientSecret,
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
