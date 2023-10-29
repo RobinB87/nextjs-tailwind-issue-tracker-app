ffe98634856ae31c7cdd4aa74efe678d32aad89a
RobinB87 <robinbette@hotmail.com>
2023-10-29 09:54:18 +0000

    populate select component

--- issue-tracker/app/issues/[id]/AssigneeSelect.tsx
+++ issue-tracker/app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import axios from "axios";
+import { User } from "next-auth";
+import React, { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Robin</Select.Item>
+          {users &&
+            users.map((x) => (
+              <Select.Item key={x.id} value={x.id}>
+                {x.name}
+              </Select.Item>
+            ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
