6d4319841f5175cc042ac8037d49155af0728ac7
RobinB87 <robinbette@hotmail.com>
2023-10-30 12:56:12 +0000

    make columns sortable

--- issue-tracker/app/issues/_components/IssueStatusFilter.tsx
+++ issue-tracker/app/issues/_components/IssueStatusFilter.tsx
@@ -5,21 +5,10 @@ import { Select } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
-  {
-    label: "All",
-  },
-  {
-    label: "Open",
-    value: "OPEN",
-  },
-  {
-    label: "In Progress",
-    value: "IN_PROGRESS",
-  },
-  {
-    label: "Closed",
-    value: "CLOSED",
-  },
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
 ];
 
 const IssueStatusFilter = () => {
