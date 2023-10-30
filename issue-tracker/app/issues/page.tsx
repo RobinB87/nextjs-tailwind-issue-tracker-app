85b64320c0136b07bc5281331dcde66e692b0abd
RobinB87 <robinbette@hotmail.com>
2023-10-30 13:04:45 +0000

    sort issues

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -19,8 +19,13 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
+  const orderBy = columns.map((x) => x.value).includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
 
   return (
